9d8ace7adc37fb65e7fac5f0e79d73af40ffda2d
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2015-08-29 14:52:45 -0500

    Update mipsOps_implement.cpp

--- mipsOps/mipsOps_implement.cpp
+++ mipsOps/mipsOps_implement.cpp
@@ -2,10 +2,6 @@
 // mipsOps_implement.cpp
 
 #include "mipsOps_head.h"
-#include<iostream>
-#include<sstream>
-#include<string>
-using namespace std;
 
 /********************************************************************
  void multiply(int a, int b)
