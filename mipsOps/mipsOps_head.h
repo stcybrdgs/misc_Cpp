ccaa1b576c3968ee428e925d5d022bd325b6f105
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2015-08-29 14:53:03 -0500

    Update mipsOps_head.h

--- mipsOps/mipsOps_head.h
+++ mipsOps/mipsOps_head.h
@@ -4,6 +4,11 @@
 #ifndef MIPSOPS_HEAD_H
 #define MIPSOPS_HEAD_H
 
+#include<iostream>
+#include<sstream>
+#include<string>
+using namespace std;
+
 // non-member function prototypes
 void printMenu();
 void multiply(int, int);
