56ac963c9e0a66934f4d25026730c2abe1bd4c92
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2015-08-29 14:41:05 -0500

    Update mipsOps_main.cpp

--- mipsOps/mipsOps_main.cpp
+++ mipsOps/mipsOps_main.cpp
@@ -9,24 +9,25 @@ Description: This program simulates how the MIPS computer architecture
              step of the multiplication or division algorithm.
 *******************************************************************************/
 #include "mipsOps_head.h"
+#include<cstdlib>
 
 int main(){
-//  declare local variables to contain user input
+    // declare local variables to contain user input
     char choice = 'M';
-    int  op_a    = 0,
-         op_b    = 0;
+    int op_a = 0,
+        op_b = 0;
 
-//  get user input and invoke simulated MIPS multiplication or division
+    // get user input and invoke simulated MIPS multiplication or division
     do{
-		printMenu( );
+	printMenu( );
         cout << "Enter menu choice: ";
         cin >> choice;
         cout << endl; 
         choice = toupper(choice);
         
         switch (choice){
-		//	invoke simulated multiplication 
-			case 'M':
+	    // invoke simulated multiplication 
+            case 'M':
                 cout << "Enter the multiplicand: ";
                 cin >> op_a;                 
                 cout << "Enter the multiplier: ";
@@ -34,8 +35,8 @@ int main(){
                 multiply(op_a, op_b);  
                 break;
 
- 		//	invoke simulated division
-			case 'D': 
+ 	    // invoke simulated division
+	    case 'D': 
                 cout << "Enter the dividend: ";
                 cin >> op_a;                 
                 cout << "Enter the divisor: ";
@@ -43,13 +44,13 @@ int main(){
                 divide(op_a, op_b);  
                 break;
 
-		//	exit the program
+	    // exit the program
             case 'Q': 
                 cout << "Goodbye." << endl;
                 break;
             default:  cout << choice << " is invalid." << endl;
-        }
+        }// end switch
     } while ((choice != 'Q'));
 
-	return 0;
-}//	end driver
+    return EXIT_SUCCESS;
+}// end driver
